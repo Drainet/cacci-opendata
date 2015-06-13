79a0f270b3e339fdecb9c3193f332f2b954d3b9e
CYJ <puzpuzbombomb@gmail.com>
2015-06-13 14:59:48 +0800

    fix bar show finished

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$("#wrapper").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("active");
-});
+// $("#wrapper").click(function(e) {
+//     e.preventDefault();
+//     $("#wrapper").toggleClass("active");
+// });
